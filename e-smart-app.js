62d19417e492cbd2ad5181f67a212a9be4da88b8
envalde <kohoutdanie@gmail.com>
2020-04-12 16:28:48 +0200

    jetzt auf jsfiddle

---
+++ e-smart-app.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: "#eapp",
+  data: {
+    queries: [
+      { hint: "Ich möchte", href: "https://www.google.de" },
+      { hint: "Ich möchte", href: "#" },
+      { hint: "Ich möchte", href: "#" },
+      { hint: "Ich möchte", href: "#" },
+      { hint: "Ich möchte", href: "#" },
+    ],
+  },
+});
---
+++ e-smart-assistent.js
@@ -0,0 +1,5 @@
+function esmartassistent() {
+  document
+    .getElementById("e-assist-window")
+    .classList.toggle("view-e-assist-window");
+}
